624354d821a0ed4d286860486cd2e05db2b5888b
AlexSnider <alexsnider8@gmail.com>
2022-09-02 16:48:22 -0300

    1.0.0

---
+++ Loguins/app.py
@@ -0,0 +1,10 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(93,883,duration=0.1)
+pyautogui.click(741,614,duration=0.7)
+pyautogui.click(707,353,duration=0.3)
+pyautogui.write("#")
+pyautogui.click(719,432,duration=0.2)
+pyautogui.write("#")
+pyautogui.click(798,481,duration=0.2)
---
+++ Loguins/app2.py
@@ -0,0 +1,10 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(93,883,duration=0.1)
+pyautogui.click(853,612,duration=0.7)
+pyautogui.click(732,717,duration=0.3)
+pyautogui.write("#")
+pyautogui.click(679,786,duration=0.2)
+pyautogui.write("#")
+pyautogui.click(788,838,duration=0.2)
