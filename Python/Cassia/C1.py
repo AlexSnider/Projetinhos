34c8406e77b724b8d0f3f111a51f273c22f2ee9b
AlexSnider <alexsnider8@gmail.com>
2022-10-23 23:12:09 -0300

    1.0.0

--- Python/Cassia/C1.py
+++ Python/Cassia/C1.py
@@ -12,20 +12,21 @@ def entrada():
         pass
     else:
         entrada()
+
 def instrucoes():
     print('Regras do jogo!!!')
+    print('Iremos rolar 2 dados e somar seus pontos para pode prosseguir.')
     print('Se na primeira jogada você tirar 7 ou 11, você vence!')
-    time.sleep(3)
+    time.sleep(0)
     print('Se você tirar 2, 3 ou 12, você perde!')
-    time.sleep(3)
+    time.sleep(0)
     print('Se na primeira jogada você tirou 4, 5, 6, 8, 9 ou 10, esse é o seu [PONTO]!')
-    time.sleep(4)
+    time.sleep(0)
     print('Seu objetivo de agora em diante é tirar novamente esse [PONTO] '
           'e se tirar um 7 antes desse ponto, você perde!')
-    time.sleep(4)
-    print('Vamos lá! Aperte ENTER para jogar os dados!')
+    time.sleep(0)
 
-def gerador():
-    return random.randint(2, 12)
+def joga_os_dados():
+    return random.randint(1, 6)
 
 entrada()
