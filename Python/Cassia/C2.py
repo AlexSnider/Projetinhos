1413e147e441b2ffbdb331a3ad52d50681c5c428
AlexSnider <alexsnider8@gmail.com>
2022-11-06 12:06:18 -0300

    1.0.0

--- Python/Cassia/C2.py
+++ Python/Cassia/C2.py
@@ -1,11 +1,18 @@
-# Desenvolva um jogo em que o jogador tenha que adivinhar uma palavra que será mostrada na tela com as letras embaralhadas.
+# Desenvolva um jogo em que o jogador tenha que adivinhar uma palavra que será mostrada na tela com as letras
+# embaralhadas.
 # No programa deverá possuir funções para:
 # - preencher uma lista de palavras (contendo no mínimo 10 palavras!)
-# - escolher uma das palavras da lista aleatoriamente (função recebe uma lista e retorna o índice da palavra selecionada na lista para o jogador adivinhar);
-# - embaralhar uma palavra passada por parâmetro – o critério de embaralhar deve ser criado pelo desenvolvedor (a função recebe a palavra que foi escolhida com a grafia correta e retorna a palavra embaralhada);
-# - jogar, onde o jogador terá seis tentativas para adivinhar a palavra (recebe a palavra escolhida com a grafia correta por parâmetro e retorna o resultado true (se o jogador venceu) ou false (se o jogador perdeu)).
+# - escolher uma das palavras da lista aleatoriamente (função recebe uma lista e retorna o índice da palavra selecionada
+# na lista para o jogador adivinhar);
+# - embaralhar uma palavra passada por parâmetro – o critério de embaralhar deve ser criado pelo desenvolvedor (a função
+# recebe a palavra que foi escolhida com a grafia correta e retorna a palavra embaralhada);
+# - jogar, onde o jogador terá seis tentativas para adivinhar a palavra (recebe a palavra escolhida com a grafia correta
+# por parâmetro e retorna o resultado true (se o jogador venceu) ou false (se o jogador perdeu)).
 #
-# Quando iniciar o jogo, o programa escolherá uma das palavras da lista (aletoriamente) e a exibirá embaralhada ao jogador. O jogador terá seis tentativas para adivinhar a palavra. Ao final, a palavra com a grafia correta deve ser mostrada na tela, informando se o jogador ganhou ou perdeu o jogo.
+# Quando iniciar o jogo, o programa escolherá uma das palavras da lista (aletoriamente) e a exibirá embaralhada ao
+# jogador.
+# O jogador terá seis tentativas para adivinhar a palavra. Ao final, a palavra com a grafia correta deve ser mostrada na
+# tela, informando se o jogador ganhou ou perdeu o jogo.
 
 
 
@@ -47,4 +54,3 @@ def embaralha(word):
 
 
 main()
-
