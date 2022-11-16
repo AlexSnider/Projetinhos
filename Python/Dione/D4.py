dc1f37dd45fc361adab74a42953476bab61f88ce
AlexSnider <alexsnider8@gmail.com>
2022-11-16 10:42:25 -0300

    1.0.0

--- Python/Dione/D4.py
+++ Python/Dione/D4.py
@@ -1,16 +1,26 @@
 # Faça um programa que, a partir de um texto digitado pelo usuário, conte o número de caracteres total e o número de palavras (palavra é definida por qualquer sequência de caracteres delimitada por espaços em branco, virgula ou ponto) e exiba o resultado.
 
-user_input = input('Digite um texto qualquer: ')
+def main():
+    user_input = input('Digite um texto qualquer: ')
+    count_caract(user_input)
+    count_words(user_input)
 
-count = 0
-contador = len(user_input)
-count += contador
 
-print('O número de total de caracteres é de:', count)
+def count_caract(user_input):
+    count = 0
+    contador = len(user_input)
+    count += contador
+    print('O número de total de caracteres é de:', count)
 
-count2 = 0
-contador2 = len(user_input.split())
-if contador2 != '' and ',' and '.':
-    count2 += contador2
 
-print('O número de total de palavras é de:', count2)
+def count_words(user_input):
+    count2 = 0
+    contador2 = len(user_input.split())
+
+    if contador2 != ',' and '.':
+        count2 += contador2
+
+    print('O número de total de palavras é de:', count2)
+
+
+main()
