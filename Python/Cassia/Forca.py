ea16ff422e0e2791ca08ff9211dc7a37f52e5cae
AlexSnider <alexsnider8@gmail.com>
2022-12-19 17:01:03 -0300

    0.2.0

--- Python/Cassia/Forca.py
+++ Python/Cassia/Forca.py
@@ -1,18 +1,34 @@
+# 1 - creat a menu that calls functions and execute it, and after, return to the menu if the game isn't called.
+# 2 - creat a function that writes random words from the user input and store it in a txt. file
+# 3 - creat a func. that loads the main file, allows to continue if the txt. file has more >= than 10 words,
+#     and if not,
+#     keep asking to the user to insert new words until it reaches 10 in total but not overwriting the first words
+#     already inserted
+# 4 - creat a func. that erases all the words in the txt. file if the user press the menu key for it
+# 5 - creat a func. that calls a hangman game using the words in the txt. file
+
 def main():
-    # user_input = int(input('Digite o número correspondente as ações dispostas a eles: '))
-    inserir_palavras()
-    carregar_arq()
+    menu()
 
 
 def menu():
-    ins_palavras = '1'
-    carr_arq = '2'
-    apagar = '3'
-    iniciar_game = '4'
-    encerrar = '5'
+    print('Welcome! Select a number to start...')
+    print('Insira o número 1 para inserir palavras no arquivo.')
+    print('Insira o número 2 para carregar o arquivo.')
+    print('Insira o número 3 para limpar o arquivo.')
+    print('Insira o número 4 para iniciar o game da forca.')
+    print('Insira o número 5 para encerrar.')
+    user_input = input('Insira o número: ')
+
+    if user_input == '1':
+        return insert_words(), load_file()
+    if user_input == '2':
+        return load_file(), menu()
+    if user_input == '3':
+        return clean_file(), menu()
 
 
-def inserir_palavras():
+def insert_words():
     print('Digite "0" e aperte ENTER para encerrar ou apenas dê um ENTER para continuar: ')
     user_input = str(input('Insira palavras para serem usadas no game da forca: '))
 
@@ -24,30 +40,34 @@ def inserir_palavras():
         user_input = str(input('Insira palavras para serem usadas no game da forca: '))
 
 
-def carregar_arq():
-    arq = open('palavras.txt', 'r', encoding='utf-8')
-    arq.read()
-    arq.seek(0, 0)
-    l_arq = arq.readlines()
+def load_file():
+    with open('palavras.txt', 'r', encoding='utf-8') as arq:
+        arq.read()
+        arq.seek(0, 0)
+        l_arq = arq.readlines()
 
-    for i in l_arq:
-        if len(i) < 10:
-            print('Você não digitou 10 palavras ainda. Continue até atingir 10!')
-            user_input = str(input('Insira palavras para serem usadas no game da forca: '))
-            l2_arq = open('palavras.txt', 'a', encoding='utf-8')
-            l2_arq.write(user_input)
-            l2_arq.write('\n')
-            print(
+        lista_arq = []
 
-                arq=open('palavras.txt', 'r', encoding='utf-8')
-            arq.read()
-            arq.seek(0, 0)
-            l2_arq = arq.readlines()
+        if len(l_arq) < 10:
+            need_lines = 10 - len(l_arq)
+            print('O arquivo parace não conter aquivos ou menos de 10...')
+            print('Você ainda precisa de mais', need_lines, 'palavras para continuar...')
+        for word in l_arq:
+            lista_arq.append(word.replace('\n', ''))
+        if need_lines != 0:
+            for i in range(need_lines):
+                with open('palavras.txt', 'a') as arq2_app:
+                    user_input = input('Insira as palavras restantes: ')
+                    arq2_app.write(user_input + '\n')
+                    lista_arq.append(user_input)
+        # for word in lista_arq:
+        #     arq.write(word + '\n')
 
-            l_palavras = []
 
-            for x in range(len(l2_arq)):
-                l_palavras.append(l2_arq[x].split())
-            print(l_palavras)
+def clean_file():
+    with open('palavras.txt', 'w') as arqs:
+        arqs.write('')
+    print('File erased!')
+
 
 main()
